412e16e49e7df4a30576f37a3209d18e1f652f57
unknown <subhash8729@gmail.com>
2026-02-17 22:00:47 +0530

    cors issue fixed

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use(router)
 app.use(cors({
   origin: "*",
   methods: ["GET","POST","PUT","DELETE","PATCH"],
-  credentials: false
+  credentials: true
 }))
+app.use(router)
 
 
 

b29f39326ddc4769cda3e524c0712469c2c47521
ajitdevv <ajitdev014@gmail.com>
2026-02-17 22:09:20 +0530

    try to fix cros in index.js file

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-import express, { urlencoded } from "express"
+// import express, { urlencoded } from "express"
+// import router from "./routes/admin.routes.js";
+// import cors from "cors"
+// import { config } from "dotenv";
+// config();
+
+// const app = express();
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+
+// app.use(router)
+// app.use(cors({
+//   origin: "*",
+//   methods: ["GET","POST","PUT","DELETE","PATCH"],
+//   credentials: false
+// }))
+
+
+
+
+
+// app.listen(3000);
+
+
+import express from "express";
 import router from "./routes/admin.routes.js";
-import cors from "cors"
+import cors from "cors";
 import { config } from "dotenv";
+
 config();
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
-app.use(router)
 app.use(cors({
   origin: "*",
   methods: ["GET","POST","PUT","DELETE","PATCH"],
-  credentials: false
-}))
+}));
 
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000);

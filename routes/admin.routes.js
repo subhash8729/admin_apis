4fc097c6a80c98bc2562d8161ff01bb0a71f81bb
ajitdevv <ajitdev014@gmail.com>
2026-02-17 19:44:05 +0530

    login and logout created

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { add_product_controller, delete_manufacturer_controller, delete_products_controller, get_manufacturers_details, get_products_controller, login_controller } from "../controllers/admin.controller.js";
 const router = express.Router();
 
-router.post("/login", login_controller);
+router.post("/log", login_controller);
 router.patch("/get-products",get_products_controller);
 router.patch("/get-manufacturers", get_manufacturers_details);
 router.delete("/delete-product", delete_products_controller)

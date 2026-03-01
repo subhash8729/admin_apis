348e8ca998d20998e32adb5939e4ad205e809f3b
subhash8729 <subhash8729@gmail.com>
2026-03-01 11:45:10 +0530

    get_deals function updated

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -108,8 +108,8 @@ export const get_deals_controller = (req, res) => {
         if (!token) return res.status(400).json({ message: "token not found" })
         const result = verifyToken(token);
         if (!result) return res.status(400).json({ message: "incorrect token" })
-
-        return res.status(200).json(deals)
+        const data = JSON.parse(fs.readFileSync("deals.json", "utf-8"));
+        return res.status(200).json(data)
     } catch (error) {
         return res.status(400).json({ message: "Server Error" })
     }

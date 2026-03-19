6c4e2c470f8586f5fb5c28ad6fcdb99e94e7a902
subhash8729 <subhash8729@gmail.com>
2026-03-19 16:17:11 +0530

    env fixes

--- functions/db.js
+++ functions/db.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 
-
 const client = new MongoClient(process.env.MONGO_URI);
 
 await client.connect();
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import cors from "cors"
 import router from "./routes/admin.routes.js";
 import cookieParser from "cookie-parser";
